52f4ce6756f052bbcbeadabb33cba59dec4af9d4
Gopalakrishna Palem <krishnapg@yahoo.com>
2015-01-07 07:29:04 +0000

    google vis loader created

---
+++ public/gloader.js
@@ -0,0 +1,9 @@
+﻿
+// This one loads the google visualization charts
+// Should have included <script>//www.google.com/jsapi</script> before this
+google.load('visualization', '1.1', {
+    packages: ['corechart', 'table'],
+    callback: function(){
+        console.log("Google Visualization Loaded");
+    }
+});
